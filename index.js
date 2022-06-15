c350f5d4721062f79958320321ef3195037339d6
Jonathas <jonathasarruda@outlook.com>
2022-06-15 14:03:55 -0300

    Adding sessionStorage on Cards

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 window.darkmode = darkmode;
 
+const storage = window.sessionStorage;
+
 function darkmode() {
   let element = document.body;
   element.classList.toggle("dark");
@@ -9,32 +11,23 @@ let cards = [];
 
 let elementoPai = document.getElementById("elementoPai");
 
-//POSTAGEM
-let Post = {
-  title: "",
-  description: "",
-  imgUrl: "",
-};
-
 let submit = document.getElementById("submit");
-
-//TESTE CONSOLE
-let verPost = () => console.log(Post);
-let verFelinosAtual = () => console.log(cards);
 
 //RENDERIZA ULTIMA OBJETO/POSTAGEM DO ARRAY
 
-let lastPost = () => {
-  let lastPostNumber = cards.length - 1;
+let renderizarPost = (postagem) => {
+  let card = `
+    <img src="${postagem.imgUrl}">
+    <h2>${postagem.title}</h2>
+    <p>${postagem.description}</p>
+  `;
 
-  let card = `<img src="${cards[lastPostNumber].imgUrl}">
-                  <h2>${cards[lastPostNumber].title}</h2>
-                  <p>${cards[lastPostNumber].description}</p>`;
   let novaDiv = document.createElement("div");
   novaDiv.className = "item";
   novaDiv.innerHTML = card;
   let firstChild = elementoPai.firstChild;
   elementoPai.insertBefore(novaDiv, firstChild);
+  
   novaDiv.addEventListener("click", () => {
     novaDiv.remove();
   });
@@ -43,12 +36,47 @@ let lastPost = () => {
 submit.addEventListener("click", (e) => {
   e.preventDefault();
 
+  //TESTE CONSOLE
+  let verPost = () => console.log(Post);
+  let verCardAtual = () => console.log(cards);
+
+  //POSTAGEM
+  let Post = {
+    title: "",
+    description: "",
+    imgUrl: "",
+  };
+
   Post.title = document.getElementById("GET-title").value;
   Post.description = document.getElementById("GET-description").value;
   Post.imgUrl = document.getElementById("GET-imageUrl").value;
 
   cards.push(Post);
   verPost();
-  verFelinosAtual();
-  lastPost();
+  verCardAtual();
+  renderizarPost(Post);
+  
+  
+  const cardsForJson =  JSON.stringify(cards)
+  
+  console.log(cardsForJson)
+  storage.setItem("cards", cardsForJson)
+  
 });
+
+
+window.onload = () => {
+
+  
+  const getItemStorage = storage.getItem("cards")
+
+ console.log(storage)
+
+ if (getItemStorage) {
+   cards = JSON.parse(getItemStorage)
+   
+   cards.forEach(elemento => {
+     renderizarPost(elemento)
+    });
+ }
+}
